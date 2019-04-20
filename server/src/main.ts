64ced2d256748101afbb748f71e6574c2a2fcda7
Rooparam Choudhary <rrc.iisc@yahoo.com>
2019-04-21 03:20:10 +0530

    merge server code in monorepo

---
+++ server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
+
+const NEST_PORT = process.env.PORT || 3000;
+const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist', 'share-market-client');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(express.static(CLIENT_FILES));
+  app.setGlobalPrefix('api');
+  await app.listen(NEST_PORT);
+}
+bootstrap();
