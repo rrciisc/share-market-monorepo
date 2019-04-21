54e03f2e50573cd8f5e6027f23fbeaa805032881
Rooparam Choudhary <rrc.iisc@yahoo.com>
2019-04-22 00:42:49 +0530

    log network requests on server

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import { NextFunction } from 'connect';
+import { performance } from "perf_hooks";
 
 const NEST_PORT = process.env.PORT || 3000;
 const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
 
+const logRequestStart = (req: Request, res, next: NextFunction) => {
+  const t0 = performance.now();
+  res.on('finish', () => {
+    const t1 = performance.now();
+    console.info(`${req.method} ${(req as any).originalUrl} | ${res.statusCode} ${res.statusMessage}; ${Math.ceil((res.get('Content-Length') || 0)/1024)} KB; ${(t1-t0).toFixed(0)} ms`);
+  });
+  next();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logRequestStart);
   app.use(express.static(CLIENT_FILES));
   app.setGlobalPrefix('api');
   await app.listen(NEST_PORT);
--- server/src/market/market.controller.ts
+++ server/src/market/market.controller.ts
@@ -6,6 +6,7 @@ export class MarketController {
 	constructor(private readonly marketService: MarketService) {}
 
 	@Get('status')
+	@Header('cache-control', 'no-store')
 	async getMarketStatus(): Promise<string> {
 		return this.marketService.getMarketStatus().then(status => {
 			return JSON.stringify(status);
@@ -13,6 +14,7 @@ export class MarketController {
 	}
 
 	@Get('symbol/:id')
+	@Header('cache-control', 'no-store')
 	@Header('content-type', 'application/json')
 	async getSymbolInfo(@Param('id') id: string): Promise<IQuoteInfo> {
 		return this.marketService.getQuoteInfo(id);
