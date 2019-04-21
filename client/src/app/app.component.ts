9a178c69376cc89c027bec33877c3d8f2c73a2c4
Rooparam Choudhary <rrc.iisc@yahoo.com>
2019-04-21 15:20:52 +0530

    client: title ux

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,5 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'share-market-client';
-
-  constructor(http: HttpClient) {
-    http.get('/api').subscribe((data: string) => {
-      setTimeout(() => {
-        this.title = data;
-      }, 3000);
-    });
-  }
+  title = 'Share Market';
 }
