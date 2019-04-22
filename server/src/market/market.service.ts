5d3aca3e407bab5d4cbd26498be93f892bb5f1b9
Rooparam Choudhary <rrc.iisc@yahoo.com>
2019-04-22 19:48:08 +0530

    server: log error when getting from nse-endpoint

--- server/src/market/market.service.ts
+++ server/src/market/market.service.ts
@@ -6,28 +6,44 @@ import { AxiosResponse } from "axios";
 export class MarketService {
 
 	async getMarketStatus(): Promise<string> {
-		return NSE.getMarketStatus().then((response: AxiosResponse<IMarketStatus>) => {
-			return response.data.status;
+		return new Promise<string>((resolve, reject) => {
+			NSE.getMarketStatus()
+			.then((response: AxiosResponse<IMarketStatus>) => {
+				resolve(response.data.status);
+			})
+			.catch(error => {
+				console.error(`getMarketStatus failed. status(${error.status}) message(${error.message})`);
+				reject(error);
+			});
 		});
 	}
 
 	async getQuoteInfo(quote: string): Promise<IQuoteInfo> {
-		return NSE.getQuoteInfo(quote).then(response => {
-			const res = response.data.data[0];
+		return new Promise<IQuoteInfo>((resolve, reject) => {
+			NSE.getQuoteInfo(quote)
+			.then(response => {
+				const res = response.data.data[0];
 
-			const buyPrice = +res.buyPrice1.replace(/,/g, '');
-			const sellPrice = +res.sellPrice1.replace(/,/g, '');
-			const lastPrice = isNaN(sellPrice) ? buyPrice : sellPrice;
-			const previousClose = +res.previousClose.replace(/,/g, '');
-
-			const info: IQuoteInfo = {
-				companyName: res.companyName,
-				symbol: res.symbol,
-				lastPrice: lastPrice.toFixed(2),
-				change: (lastPrice - previousClose).toFixed(2),
-				deliveryPercentage: (+res.deliveryToTradedQuantity).toFixed(2)
-			};
-			return info;
+				const closePrice = +res.closePrice.replace(/,/g, '');
+				const buyPrice = +res.buyPrice1.replace(/,/g, '');
+				const sellPrice = +res.sellPrice1.replace(/,/g, '');
+				const lastPrice = isNaN(closePrice) ? (isNaN(sellPrice) ? buyPrice : sellPrice) : closePrice;
+				const previousClose = +res.previousClose.replace(/,/g, '');
+	
+				const info: IQuoteInfo = {
+					companyName: res.companyName,
+					symbol: res.symbol,
+					lastPrice: lastPrice.toFixed(2),
+					change: (lastPrice - previousClose).toFixed(2),
+					deliveryPercentage: (+res.deliveryToTradedQuantity).toFixed(2)
+				};
+	
+				return resolve(info);
+			})
+			.catch(error => {
+				console.error(`getQuoteInfo failed for '${quote}'. status(${error.status}) message(${error.message})`);
+				reject(error);
+			});
 		});
 	}
 }
